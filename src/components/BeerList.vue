4379994505bd347ac5c424c1f31a79816f02706e
Antoine C <antoine.cesbron.perso@gmail.com>
2023-05-14 01:05:40 +0200

    cleaning

--- src/components/BeerList.vue
+++ src/components/BeerList.vue
@@ -28,13 +28,14 @@
       </div>
     </div>
     <ul>
-      <li v-for="beer in filteredBeers" :key="beer.id">
+      <li v-for="beer in sortedBeers" :key="beer.id">
         <div class="beer-item" @click="goToBeer(beer.id)">
           <div class="beer-info">
-            <p>{{ beer.name }} - {{ beer.price.toFixed(2) }}€</p>
+            <p>
+              {{ beer.name }} - <strong>{{ beer.price.toFixed(2) }}€</strong>
+            </p>
           </div>
           <div class="beer-image">
-            <!-- <img :src="beer.beerImage.imageUrl" alt="Beer image" /> -->
             <img :src="beer.imageUrl" alt="Beer image" />
           </div>
         </div>
@@ -58,6 +59,22 @@ export default {
       maxPrice: null,
     };
   },
+  computed: {
+    sortedBeers() {
+      return this.filteredBeers.slice().sort((beer1, beer2) => {
+        const name1 = beer1.name.toLowerCase();
+        const name2 = beer2.name.toLowerCase();
+
+        if (name1 < name2) {
+          return -1;
+        } else if (name1 > name2) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+    },
+  },
   methods: {
     async fetchBreweries() {
       try {
@@ -69,6 +86,7 @@ export default {
     },
     filterBeers() {
       let filtered = this.beers;
+
       if (this.selectedBrewery) {
         filtered = filtered.filter(
           (beer) => beer.breweryId === this.selectedBrewery
@@ -77,18 +95,20 @@ export default {
 
       const minPrice = parseFloat(this.minPrice);
       const maxPrice = parseFloat(this.maxPrice);
-      if (!isNaN(minPrice) || !isNaN(maxPrice)) {
-        filtered = filtered.filter((beer) => {
-          const beerPrice = parseFloat(beer.price);
-          if (isNaN(minPrice)) {
-            return beerPrice <= maxPrice;
-          } else if (isNaN(maxPrice)) {
-            return beerPrice >= minPrice;
-          } else {
-            return beerPrice >= minPrice && beerPrice <= maxPrice;
-          }
-        });
-      }
+
+      filtered = filtered.filter((beer) => {
+        const beerPrice = parseFloat(beer.price);
+
+        if (!isNaN(minPrice) && beerPrice < minPrice) {
+          return false;
+        }
+
+        if (!isNaN(maxPrice) && beerPrice > maxPrice) {
+          return false;
+        }
+
+        return true;
+      });
 
       this.filteredBeers = filtered;
     },
