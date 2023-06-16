814a2e61df5568ed2a3f93f1410d87b4a2bb6033
Curzeed <87865741+Curzeed@users.noreply.github.com>
2023-06-16 09:35:29 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://frozen-lake-57865.herokuapp.com/api',
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://frozen-lake-57865.herokuapp.com/api',
+    // baseURL: 'http://localhost:3000/api',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
